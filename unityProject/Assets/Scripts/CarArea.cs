452657928f8914ed87005c7be733fe3352413c37
Ethan Bruins <ethan.bruins@unity3d.com>
2020-07-10 22:04:31 +0100

    base script for vehicle added

---
+++ unityProject/Assets/Scripts/CarArea.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarArea : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Duck"))
+        {
+            //check if its user controlled, if it is we need to wait.
+        }
+    }
+}
