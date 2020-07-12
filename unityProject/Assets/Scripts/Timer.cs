c9ecdb5a5b40a722561fa66d31e9769912a0b026
Ethan Bruins <ethan.bruins@unity3d.com>
2020-07-12 18:32:09 +0100

    Timer added

---
+++ unityProject/Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float totalTime;
+    float currTime;
+    public CarScript car;
+
+    public TMP_Text timerText;
+    bool counting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counting = true;
+        currTime = totalTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!counting)
+            return;
+        currTime -= Time.deltaTime;
+        timerText.text = currTime.ToString("F1");
+
+        if(currTime <= 0)
+        {
+            counting = false;
+            car.GameOver();
+        }
+    }
+}
