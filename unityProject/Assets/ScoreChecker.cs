03611ae74bcf16e5a54cc75f71cf46caa6e0073c
Jacek <jacek@unity3d.com>
2020-07-12 18:39:24 +0100

    PlayerPrefs + End Scene
    donezo

---
+++ unityProject/Assets/ScoreChecker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreChecker : MonoBehaviour
+{
+    TMP_Text scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "Your BEST BREADS: " + PlayerPrefs.GetInt("Breads").ToString();
+    }
+}
