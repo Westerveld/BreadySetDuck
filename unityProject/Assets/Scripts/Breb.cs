0edd5914d5550c7a711215c0a009d08fe92f215f
Jacek <jacek@unity3d.com>
2020-07-12 14:57:44 +0100

    Breb
    yummy

---
+++ unityProject/Assets/Scripts/Breb.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breb : MonoBehaviour
+{
+
+    public bool isSpinning;
+    public float spinSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSpinning)
+            transform.Rotate(Vector3.forward * Time.deltaTime * spinSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "CarCollectionZone")
+        {
+            Debug.Log("Breb collected");
+            Destroy(this.gameObject);
+        }
+    }
+}
