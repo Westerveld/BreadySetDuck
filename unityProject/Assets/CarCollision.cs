7aa225c0b20c865174fe1e581367c1302c0a26df
Jacek <jacek@unity3d.com>
2020-07-12 15:48:19 +0100

    health stuff

---
+++ unityProject/Assets/CarCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCollision : MonoBehaviour
+{
+    public GameObject carParent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Obstacles"))
+        {
+            Debug.Log("collided with obstacles");
+            carParent.GetComponent<CarScript>().OofOwwieOuchie();
+        }
+    }
+}
